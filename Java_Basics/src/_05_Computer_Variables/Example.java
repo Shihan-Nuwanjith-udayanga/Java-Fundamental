caa1f1a4c3c9041e7bc2a062fb00580f48f0b6d8
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-04-06 17:17:19 +0530

    Computer Variables (How to Create Variables)

---
+++ Java_Basics/src/_05_Computer_Variables/Example.java
@@ -0,0 +1,196 @@
+package _05_Computer_Variables;
+
+// ====================== Computer Variables =====================
+
+/*
+class Example{
+    public static void main(String args[]){
+        int x; //Create a computer variable--> Variable declaration
+        x=100; //Assign 100 to variable x  --> Variable initialization
+        System.out.println(x); //print value of x
+    }
+}
+*/
+
+// Exercise 01
+/*
+class Example{
+    public static void main(String args[]){
+        int x;
+        x=100;
+        System.out.println(x); // Prints 100
+        System.out.println('x'); // Prints x
+        System.out.println("x"); // Prints x
+    }
+}
+*/
+
+// Exercise 02
+/*
+class Example{
+    public static void main(String args[]){
+        int x;
+        //x=100;
+        System.out.println(x); //error: variable x might not have been initialize
+    }
+}
+*/
+
+// Exercise 03
+/*
+class Example{
+    public static void main(String args[]){
+        int x;
+        System.out.println(x); //Illegal
+        x=100;
+    }
+}
+*/
+
+// Exercise 04
+/*
+class Example{
+    public static void main(String args[]){
+        int x;
+        x=100;
+        x=200;
+        System.out.println(x);//prints 200
+    }
+}
+*/
+
+// Exercise 05
+/*
+class Example{
+    public static void main(String args[]){
+        int x;
+        x=100;
+        System.out.println(x); //prints 100
+        x=200;  //prints 200
+        System.out.println(x);
+    }
+}
+*/
+
+// Exercise 06
+/*
+class Example{
+    public static void main(String args[]){
+        int x;
+        x=100;
+        System.out.println(x); //prints 100
+        //
+        //
+        int y;
+        y=200;
+        System.out.println(y); // Prints 200
+    }
+}
+*/
+
+// Exercise 07
+/*
+class Example{
+    public static void main(String args[]){
+        int x,y;
+        x=100;
+        y=200;
+        System.out.println(x); //prints 100
+        System.out.println(y); //prints 200
+    }
+}
+*/
+
+// Exercise 08
+/*
+class Example{
+    public static void main(String args[]){
+        int x=100;
+        int y=200;
+        System.out.println(x); //prints 100
+        System.out.println(y); //prints 200
+    }
+}
+*/
+
+// Exercise 09
+/*
+class Example{
+    public static void main(String args[]){
+        int x,y,z;
+        x=y=z=100;
+        System.out.println(x); //prints 100
+        System.out.println(y); //prints 100
+        System.out.println(z); //prints 100
+    }
+}
+*/
+
+// Exercise 10
+/*
+class Example{
+    public static void main(String args[]){
+        int x=100,y,z=200;
+        y=300;
+        System.out.println(x); //prints 100
+        System.out.println(y); //prints 200
+        System.out.println(z); //prints 300
+    }
+}
+*/
+
+// Exercise 11
+/*
+class Example{
+    public static void main(String args[]){
+        int x=100,y,z=200;
+        y=300;
+        System.out.println(x); //prints 100
+        System.out.println(y); //prints 200
+        System.out.println(z); //prints 300
+
+        int x=10; //Illegal ?
+        System.out.println(x); //prints 10
+
+    }
+}
+*/
+
+// Exercise 12
+/*
+class Example{
+    public static void main(String args[]){
+        int x=100,y=200;
+        System.out.println(x); //prints 100
+        System.out.println(y); //prints 200
+
+        x=y;
+        System.out.println(x); // prints 200
+        System.out.println(y); // prints 200
+
+    }
+}
+*/
+
+// Exercise 13
+/*
+class Example{
+    public static void main(String args[]){
+        int x=100,y=200;
+        int z;
+        z=x+y;
+        System.out.println(z); //prints 300
+    }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
