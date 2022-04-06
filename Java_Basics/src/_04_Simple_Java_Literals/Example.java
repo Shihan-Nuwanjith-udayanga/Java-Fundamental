9b255786902ac45ae56734139aaf841a8ed3ce59
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-04-06 17:03:38 +0530

    Simple Java Literals

---
+++ Java_Basics/src/_04_Simple_Java_Literals/Example.java
@@ -0,0 +1,87 @@
+package _04_Simple_Java_Literals;
+
+// =================== Simple Java Literals ============================
+
+/*
+        There are,
+            01. Integer Literals (100 , 126 , -725 )
+            02. Floating-Point Literals (1.256 , -0.256)
+            03. Character Literals ('A' , 'B')
+            04. String Literals ("Hello" , "7" , "A")
+            05. Boolean literals (true , false)
+*/
+
+// 01. Integer Literals
+/*
+class Example{
+    public static void main(String args[]){
+        System.out.println(100);
+        System.out.println(123);
+        System.out.println(-1234);
+    }
+}
+*/
+
+// 02. Floating-Point Literals
+/*
+class Example{
+    public static void main(String args[]){
+        System.out.println(100.0012);
+        System.out.println(0.00122);
+        System.out.println(-0.00122);
+    }
+}
+*/
+
+// 03. Character Literals
+/*
+class Example{
+    public static void main(String args[]){
+        System.out.println('A');
+        System.out.println('6');
+        System.out.println('$');
+    }
+}
+*/
+
+//04. String Literals
+/*
+class Example{
+    public static void main(String args[]){
+        System.out.println("Hello");
+        System.out.println("H");
+        System.out.println("1234");
+        System.out.println("7");
+    }
+}
+
+*/
+
+//05. Boolean literals
+/*
+class Example{
+    public static void main(String args[]){
+        System.out.println(true);
+        System.out.println(false);
+
+    }
+}
+*/
+
+// =====================================
+/*
+class Example{
+    public static void main(String args[]){
+        System.out.println('AB'); //Illegal Character
+        System.out.println(True); // Illegal ?
+
+    }
+}
+*/
+
+
+
+
+
+
+
