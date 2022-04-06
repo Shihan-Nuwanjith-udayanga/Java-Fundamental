ca7e65c36e0cf4db4a0c06f485260d8372a2b7f4
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-04-06 16:36:36 +0530

    System.out.println(data) vs System.out.print(data)

---
+++ Java_Basics/src/Println_VS_Print/Example.java
@@ -0,0 +1,53 @@
+package Println_VS_Print;
+
+// ========= System.out.println(data) vs System.out.print(data) ============
+
+//01
+/*
+class Example {
+    public static void main(String[] args) {
+        System.out.print("A");
+        System.out.print("B");
+        System.out.print("C");
+        System.out.print("D");
+    }
+}
+*/
+
+//output - ABCD
+
+//02
+/*
+ class Example{
+    public static void main(String[] args) {
+        System.out.println("A");
+        System.out.print("B");
+        System.out.print("C");
+        System.out.println("D");
+    }
+}
+*/
+
+/*
+output - A
+         BCD
+*/
+
+//03
+/*
+class Example{
+    public static void main(String args[]){
+        System.out.print("A");
+        System.out.println("B");
+        System.out.println("C");
+        System.out.print("D");
+    }
+}
+*/
+
+/*
+output - AB
+         C
+         D
+*/
+
