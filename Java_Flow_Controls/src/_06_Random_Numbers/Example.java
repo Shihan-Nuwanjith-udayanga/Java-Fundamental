fb90bf01de7d4cbcfc5e213197c4b83b509a659f
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-04-12 11:23:11 +0530

    Random Numbers

---
+++ Java_Flow_Controls/src/_06_Random_Numbers/Example.java
@@ -0,0 +1,34 @@
+package _06_Random_Numbers;
+
+// =============================== Random Numbers =====================================
+
+/*
+import java.util.*;
+class Example{
+    public static void main(String args[]){
+        Random input=new Random();
+        int num=input.nextInt(); //min of int to max of int
+        //
+        System.out.println(num);
+    }
+}
+*/
+
+// Example
+/*
+import java.util.*;
+class Example{
+    public static void main(String args[]){
+        Random input=new Random();
+        int num=input.nextInt();
+        int abs;
+        if(num<0){
+            abs=-num;
+        }else{
+            abs=num;
+        }
+        System.out.println(num+" "+abs);
+    }
+}
+*/
+
