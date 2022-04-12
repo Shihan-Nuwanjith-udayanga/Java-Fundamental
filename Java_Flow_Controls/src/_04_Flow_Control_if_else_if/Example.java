9d06f9e2931c7ffbea13c618f7408cf71e4c3a55
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-04-12 11:12:20 +0530

    Java Flow Control "if/else if" (if ladder)

---
+++ Java_Flow_Controls/src/_04_Flow_Control_if_else_if/Example.java
@@ -0,0 +1,54 @@
+package _04_Flow_Control_if_else_if;
+
+// ================================ Java Flow Control "if/else if" (if ladder) ==============================
+
+// Q-01
+/*
+import java.util.Scanner;
+class Example {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        double avg;
+        System.out.print("Input average marks  : ");
+        avg=input.nextDouble(); //74.9
+        if(avg>=75){
+            System.out.println("Grade A");
+        }
+        if(avg>=65){
+            System.out.println("Grade B");
+        }
+        if(avg>=45){
+            System.out.println("Grade C");
+        }
+        if(avg>=25){
+            System.out.println("Grade D");
+        }else{
+            System.out.println("Grade E");
+        }
+    }
+}
+*/
+
+// Q-02 From Q-01
+/*
+import java.util.Scanner;
+class Example {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        double avg;
+        System.out.print("Input average marks  : ");
+        avg=input.nextDouble(); //74.9
+        if(avg>=75){
+            System.out.println("Grade A");
+        }else if(avg>=65){
+            System.out.println("Grade B");
+        }else if(avg>=45){
+            System.out.println("Grade C");
+        }else if(avg>=25){
+            System.out.println("Grade D");
+        }else{
+            System.out.println("Grade E");
+        }
+    }
+}
+*/
