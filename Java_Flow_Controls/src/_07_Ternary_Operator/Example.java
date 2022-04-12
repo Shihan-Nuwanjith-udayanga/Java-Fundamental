bf1109cd924e69444d9c46ffaa6069bdc797cc73
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-04-12 11:26:03 +0530

    Ternary Operator "?: " (conditional operator, if then else operator)

---
+++ Java_Flow_Controls/src/_07_Ternary_Operator/Example.java
@@ -0,0 +1,26 @@
+package _07_Ternary_Operator;
+
+// ============= Ternary Operator "?: " (conditional operator, if then else operator) ==============
+
+/*
+import java.util.*;
+class Example{
+    public static void main(String args[]){
+        //Scanner input=new Scanner(System.in);
+        Random input=new Random();
+        int num=input.nextInt();
+        int abs;
+		*/
+/*
+		if(num<0){
+			abs=-num;
+		}else{
+			abs=num;
+		}
+		*//*
+
+        abs=num<0 ? -num : num;
+        System.out.println(num+" "+abs);
+    }
+}
+*/
