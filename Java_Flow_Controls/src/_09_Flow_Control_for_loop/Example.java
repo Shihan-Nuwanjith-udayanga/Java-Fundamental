2fa81c8d9cd1cfc1cbd56f5edee7a5b69cd8a7ba
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-04-13 18:50:08 +0530

    Java Flow Control "for-loop"

---
+++ Java_Flow_Controls/src/_09_Flow_Control_for_loop/Example.java
@@ -0,0 +1,80 @@
+package _09_Flow_Control_for_loop;
+
+// ======================= Java Flow Control "for-loop" ============================
+
+//step 01
+/*
+class Example{
+    public static void main(String args[]){
+        System.out.println("Hello java");
+        System.out.println("Hello java");
+        System.out.println("Hello java");
+        System.out.println("Hello java");
+        System.out.println("Hello java");
+        System.out.println("Hello java");
+        System.out.println("Hello java");
+        System.out.println("Hello java");
+        System.out.println("Hello java");
+        System.out.println("Hello java");
+    }
+}
+*/
+
+//step 02
+/*
+class Example{
+    public static void main(String args[]){
+        for(int i=0; i<10; i++) System.out.println("Hello java");
+    }
+}
+*/
+
+//step 03
+/*
+class Example{
+    public static void main(String args[]){
+        System.out.println("Start");
+        for(int i=0; i<10; i++) System.out.println("Hello java");
+        System.out.println("End.");
+    }
+}
+*/
+
+//step 04
+/*
+class Example{
+    public static void main(String args[]){
+        System.out.println("Start");
+        for(int i=0; i<10; i++)
+            System.out.println("Hello java");
+        System.out.println("End.");
+    }
+}
+*/
+
+//step 05
+/*
+class Example{
+    public static void main(String args[]){
+        System.out.println("Start");
+        for(int i=0; i<10; i++)
+            System.out.println("Hello");
+        System.out.println("Java");
+        System.out.println("End.");
+    }
+}
+*/
+
+//step 06
+/*
+class Example{
+    public static void main(String args[]){
+        System.out.println("Start");
+        for(int i=0; i<10; i++){
+            System.out.println("Hello");
+            System.out.println("Java");
+        }
+        System.out.println("End.");
+    }
+}
+*/
