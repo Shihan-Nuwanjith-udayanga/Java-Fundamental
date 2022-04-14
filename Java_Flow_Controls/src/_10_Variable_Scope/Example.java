ac301289f59b09639c185c4df5667d881a83f389
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-04-14 20:06:36 +0530

    Variable Scope and lifetime Exercises

--- Java_Flow_Controls/src/_10_Variable_Scope/Example.java
+++ Java_Flow_Controls/src/_10_Variable_Scope/Example.java
@@ -214,3 +214,200 @@ class Example{
 }
 */
 
+//===========================================================================================================
+
+//Exercise 1
+/*
+class Example{
+    public static void main(String args[]){
+        for(int i=0; i<100;i++){
+            System.out.println(i);
+        }
+    }
+}
+*/
+
+//Exercise 2
+/*
+class Example{
+    public static void main(String args[]){
+        for(int i=0; i<100;i++){
+            System.out.println(i+1);
+        }
+    }
+}
+*/
+
+//Exercise 3 From Exercise 2
+/*
+class Example{
+    public static void main(String args[]){
+        for(int i=1; i<101;i++){
+            System.out.println(i);
+        }
+    }
+}
+*/
+
+//Exercise 4
+/*
+class Example{
+    public static void main(String args[]){
+        for(int i=100; i>0;i--){
+            System.out.println(i);
+        }
+    }
+}
+*/
+
+//Exercise 5
+/*
+class Example{
+    public static void main(String args[]){
+        for(int i=0; i<10;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
+*/
+
+//Exercise 6
+/*
+class Example{
+    public static void main(String args[]){
+        System.out.print("[");
+        for(int i=0; i<10;i++){
+            System.out.print(i+", ");
+        }
+        System.out.println("\b\b]");
+    }
+}
+*/
+
+//Exercise 7
+/*
+import java.util.*;
+class Example{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.print("Input a digit : ");
+        int digit=input.nextInt();
+        System.out.print("[");
+        for(int i=0; i<10;i++){
+            System.out.print(digit+", ");
+        }
+        System.out.println("\b\b]");
+    }
+}
+*/
+
+//Exercise 8
+/*
+import java.util.*;
+class Example{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.print("Input an integer  : ");
+        int x=input.nextInt();
+        System.out.print("[");
+        for(int i=1; i<=x;i++){
+            System.out.print(i+", ");
+        }
+        System.out.println("\b\b]");
+    }
+}
+*/
+
+//Exercise 9
+/*
+import java.util.*;
+class Example{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.print("Input an integer  : ");
+        int x=input.nextInt();
+        System.out.print("[");
+        for(int i=x; i>0;i--){
+            System.out.print(i+", ");
+        }
+        System.out.println("\b\b]");
+    }
+}
+*/
+
+//Exercise 10
+/*
+import java.util.*;
+class Example{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.print("Input an integer  : ");
+        int x=input.nextInt();
+
+        for(int i=1; i<=x;i++){
+            System.out.print(i+" + ");
+        }
+        System.out.println("\b\b = ");
+    }
+}
+*/
+
+//Exercise 11
+/*
+import java.util.*;
+class Example{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.print("Input an integer  : ");
+        int x=input.nextInt();
+        int tot=0;
+        for(int i=1; i<=x;i++){
+            System.out.print(i+" + ");
+            tot+=i;
+        }
+        System.out.println("\b\b = "+tot);
+    }
+}
+*/
+
+//Exercise 12
+/*
+import java.util.*;
+class Example{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.print("Input an integer  : ");
+        int x=input.nextInt();
+        int fact=1;
+        for(int i=x; i>0;i--){
+            System.out.print(i+" * ");
+            fact*=i;
+        }
+        System.out.println(" = "+fact);
+    }
+}
+*/
+
+//Exercise 13
+/*
+import java.util.*;
+class Example{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.print("Input no of students : ");
+        final int N=input.nextInt();
+        int marks;
+        int total=0;
+
+        for(int i=0; i<N; i++){
+            System.out.print("Input marks "+(i+1)+" : " );
+            marks=input.nextInt();
+            total=total+marks;
+        }
+        double avg=(double)total/N;
+        System.out.println("Total   : "+total);
+        System.out.println("Average : "+avg);
+    }
+}
+*/
+
