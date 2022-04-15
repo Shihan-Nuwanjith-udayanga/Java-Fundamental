01884f2d26a335041433ffeb29c433486701bd6a
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-04-15 16:02:22 +0530

    Nested Loop

---
+++ Java_Flow_Controls/src/_13_Nested_Loop/Example.java
@@ -0,0 +1,49 @@
+package _13_Nested_Loop;
+
+// =================================== Nested Loop ==============================================
+
+/*
+class Example{
+    public static void main(String args[]){
+        System.out.println("Start");
+        for (int i = 0; i < 10; i++){
+            for(int j=0; j<10; j++){
+                System.out.println(i+" "+j);
+            }
+        }
+        System.out.println("End.");
+    }
+}
+*/
+
+
+/*
+class Example{
+    public static void main(String args[]){
+        System.out.println("Start");
+        for (int i = 0; i < 10; i++){
+            for(int j=0; j<10; j++){
+                System.out.println(i+" "+j);
+            }
+            System.out.println("======");
+        }
+        System.out.println("End.");
+    }
+}
+*/
+
+
+/*
+class Example{
+    public static void main(String args[]){
+        System.out.println("Start");
+        for (int i = 10; i > 0; i--){
+            for(int j=10; j>0; j--){
+                System.out.println(i+" "+j);
+            }
+            System.out.println("=====");
+        }
+        System.out.println("End.");
+    }
+}
+*/
