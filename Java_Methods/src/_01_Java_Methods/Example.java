a1a327d97fccdab52a6988110bc68e9786a1cb11
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-04-16 07:39:42 +0530

    Create Java Methods Module & Example.java file

---
+++ Java_Methods/src/_01_Java_Methods/Example.java
@@ -0,0 +1,56 @@
+package _01_Java_Methods;
+
+// ===================================== Java Methods (Functions) ===================================
+
+//Step 01
+/*
+class Example{
+    public static void main(String args[]){
+        System.out.println("Niroth Samarawickrama,");
+        System.out.println("No 500/10, ");
+        System.out.println("Galle Rd, ");
+        System.out.println("Panadura.");
+
+
+        System.out.println("Niroth Samarawickrama,");
+        System.out.println("No 500/10, ");
+        System.out.println("Galle Rd, ");
+        System.out.println("Panadura.");
+
+        System.out.println("Niroth Samarawickrama,");
+        System.out.println("No 500/10, ");
+        System.out.println("Galle Rd, ");
+        System.out.println("Panadura.");
+    }
+}
+*/
+
+//Step 02
+/*
+class Example{
+    //----------------Start method declaration-----------
+    public static void printMyAddress(){
+        System.out.println("Niroth Samarawickrama,");
+        System.out.println("No 500/10, ");
+        System.out.println("Galle Rd, ");
+        System.out.println("Panadura.");
+    }
+    //----------------Start method declaration-----------
+
+    //-----------------start main method declaration-----
+    public static void main(String args[]){
+        printMyAddress(); //Method Calling statement (/run/execute)
+
+        //
+        //
+        //
+        printMyAddress();
+    }
+    //-------------------end main method declaration-----
+}
+*/
+
+
+//=========================================================================================================
+
+
