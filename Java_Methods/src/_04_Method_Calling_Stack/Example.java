a36c63aeaa0614e2f3632e265c2ee63567a00adb
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-04-19 07:49:55 +0530

    Method Calling Stack Exercise

--- Java_Methods/src/_04_Method_Calling_Stack/Example.java
+++ Java_Methods/src/_04_Method_Calling_Stack/Example.java
@@ -73,3 +73,59 @@ class Example{
     }
 }
 */
+
+
+//======================================================================================================
+
+//Exercise (++x vs x++)
+/*
+import java.util.*;
+class Example{
+    public static int increment(int x){
+        System.out.println(x);
+        x++;
+        System.out.println(x);
+        return x;
+    }
+    public static void main(String args[]){
+        int x=100;
+        System.out.println(x);
+        increment(x++);
+        System.out.println(x);
+        x=increment(++x);
+        System.out.println(x);
+    }
+}
+*/
+
+//Exercise (++x vs x++)
+/*
+import java.util.*;
+class Example{
+    public static int increment(int x){
+        return x++;
+    }
+    public static void main(String args[]){
+        int x=100;
+        System.out.println(x);
+        x=increment(x++);
+        System.out.println(x);
+    }
+}
+*/
+
+//Exercise (++x vs x++)
+/*
+import java.util.*;
+class Example{
+    public static int increment(int x){
+        return ++x;
+    }
+    public static void main(String args[]){
+        int x=100;
+        System.out.println(x);
+        x=increment(x++);
+        System.out.println(x);
+    }
+}
+*/
