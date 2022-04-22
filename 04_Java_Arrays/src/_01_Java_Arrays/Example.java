e61b0ea0051534ab139e4ac05d5d7eae7e187fb2
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-04-22 14:03:33 +0530

    Java Arrays Exercise

--- 04_Java_Arrays/src/_01_Java_Arrays/Example.java
+++ 04_Java_Arrays/src/_01_Java_Arrays/Example.java
@@ -167,3 +167,112 @@ class Example{
 }
 */
 
+
+// ===========================================================================================================
+
+//Exercise 01
+// Step 01
+/*
+class Example{
+    public static void main(String args[]){
+        int a=23,b=43,c=14,d=34;
+
+        int max=a;
+        if(b>max){
+            max=b;
+        }
+        if(c>max){
+            max=c;
+        }
+        if(d>max){
+            max=d;
+        }
+        System.out.println("Max   : "+max);
+    }
+}
+*/
+
+// Step 02 ( Using Array)
+/*
+class Example{
+    public static void main(String args[]){
+        //int a=23,b=43,c=14,d=34;
+        int[] x=new int[4];
+        x[0]=23;
+        x[1]=43;
+        x[2]=44;
+        x[3]=34;
+
+        int max=x[0];
+        for(int i=1 ; i<4; i++){
+            if(x[i]>max){
+                max=x[i];
+            }
+        }
+
+        System.out.println("Max   : "+max);
+    }
+}
+*/
+
+
+// ===========================================================================================================
+
+//Exercise 02
+/*
+import java.util.*;
+class Example{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Input number of students : " );
+        final int N=input.nextInt();
+
+        //1. Create an array named "marks" to to students marks
+        int[] marks=new int[N];
+
+        //2. Read marks (Random numbers 0 to 100)
+        Random r=new Random();
+        for (int i = 0; i < N; i++){
+            marks[i]=r.nextInt(101); //0 to 100
+        }
+
+        //3. Find total marks
+        int total=0;
+        for (int i = 0; i < N; i++){
+            total+=marks[i];
+        }
+
+        //4. Find maximum marks
+        int max=marks[0];
+        for (int i = 1; i < N; i++)	{
+            if(marks[i]>max){
+                max=marks[i];
+            }
+        }
+
+        //5. find minimum marks
+        int min=marks[0];
+        for (int i = 1; i < N; i++){
+            if(marks[i]<min){
+                min=marks[i];
+            }
+        }
+
+        //6. print marks as [54,65,34,65,34.......]
+        System.out.print("[");
+        for (int i = 0; i < N; i++){
+            System.out.print(marks[i]+", ");
+        }
+        System.out.println("\b\b]");
+
+
+        System.out.println("Total : "+total);
+        System.out.println("Max   : "+max);
+        System.out.println("Min   : "+min);
+    }
+}
+*/
+
+// ===========================================================================================================
+
