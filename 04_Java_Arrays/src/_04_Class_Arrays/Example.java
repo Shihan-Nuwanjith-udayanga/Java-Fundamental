b07aeeca339655d3b35db999383b3cd7186e174d
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-04-23 15:50:37 +0530

    Class "Arrays"

---
+++ 04_Java_Arrays/src/_04_Class_Arrays/Example.java
@@ -0,0 +1,61 @@
+package _04_Class_Arrays;
+
+// ======================================= Class "Arrays" ===========================================
+
+// Method "public static String toString(array)"
+// ============================================
+
+/*
+import java.util.*;
+public class Example {
+    public static void main(String[] args) {
+        int[] xr={89,78,12,54,12,5,7,67,78,84,99};
+        String list=Arrays.toString(xr);
+        System.out.println(list);
+        System.out.println(Arrays.toString(xr));
+
+        char[] grade={'A','B','C','D','E','F'};
+        System.out.println(Arrays.toString(grade));
+    }
+}
+*/
+
+
+// Method "public static void sort(array)"
+// =======================================
+
+/*
+import java.util.*;
+public class Example {
+    public static void main(String[] args) {
+        int[] xr={89,78,12,54,12,5,7,67,78,84,99};
+        System.out.println(Arrays.toString(xr));
+        Arrays.sort(xr);
+        System.out.println(Arrays.toString(xr));
+    }
+}
+*/
+
+
+// Method "public static int binarySearch(array, element)"
+// =======================================================
+
+/*
+import java.util.*;
+public class Example {
+    public static void main(String[] args) {
+        int[] xr={89,78,12,54,12,5,7,67,71,84,99};
+        System.out.println(Arrays.toString(xr));
+        Arrays.sort(xr);
+        System.out.println(Arrays.toString(xr));
+
+        int index;
+        index=Arrays.binarySearch(xr,54);
+        System.out.println("Index of 54 : "+index);
+
+        index=Arrays.binarySearch(xr,100);
+        System.out.println("Index of 100: "+index);
+    }
+}
+*/
+
