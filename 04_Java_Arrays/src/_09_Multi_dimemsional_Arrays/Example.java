7baee2f509fc7194406f2691c7efd13c8cf05e92
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-04-26 07:37:19 +0530

    Multi-dimensional Arrays Exercise

--- 04_Java_Arrays/src/_09_Multi_dimemsional_Arrays/Example.java
+++ 04_Java_Arrays/src/_09_Multi_dimemsional_Arrays/Example.java
@@ -364,3 +364,83 @@ public class Example {
     }
 }
 */
+
+
+// ===============================================================================================
+
+//Exercise 01
+/*
+import java.util.*;
+public class Example {
+    public static int[][] createTwoDArray(int size){
+        Scanner input=new Scanner(System.in);
+        int[][] marks=new int[size][];
+        for (int i = 0; i < marks.length; i++){
+            System.out.print("Input no of subjects for student "+(i+1)+" : ");
+            int s=input.nextInt();
+            marks[i]=new int[s];
+        }
+        return marks;
+    }
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Input no of students : ");
+        final int N=input.nextInt();
+
+        //1. create twoD array to input marks
+        int[][] marks=createTwoDArray(N);
+
+        //2. Read marks (Random numbers 0 to 100)
+        readMarks(marks);
+
+        //3. Print marks table
+        printMarks(marks);
+    }
+}
+*/
+
+//Exercise 02 From 01
+/*
+public class Example {
+    public static int[][] createTwoDArray(int size){
+        Scanner input=new Scanner(System.in);
+        int[][] marks=new int[size][];
+        for (int i = 0; i < marks.length; i++){
+            System.out.print("Input no of subjects for student "+(i+1)+" : ");
+            int s=input.nextInt();
+            marks[i]=new int[s];
+        }
+        return marks;
+    }
+    public static void readMarks(int[][] x){
+        Random r=new Random();
+        for(int i=0; i<x.length; i++){
+            for(int j=0; j<x[i].length; j++){
+                x[i][j]=r.nextInt(101);
+            }
+        }
+    }
+    public static void printMarks(int[][] f){
+        for(int i=0; i<f.length; i++){
+            for(int j=0; j<f[i].length; j++){
+                System.out.print(f[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Input no of students : ");
+        final int N=input.nextInt();
+
+        //1. create twoD array to input marks
+        int[][] marks=createTwoDArray(N);
+
+        //2. Read marks (Random numbers 0 to 100)
+        readMarks(marks);
+
+        //3. Print marks table
+        printMarks(marks);
+    }
+}
+*/
